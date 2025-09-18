073be5e2f971deed92414ae1a1c05b1788be6358
=William <ruyk1120@gmail.com>
2025-09-18 08:53:43 -0500

    Archivo inicial del proyecto

---
+++ src/test/java/co/edu/uptc/temufacebook/TemufacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.temufacebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemufacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
